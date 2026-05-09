5594f5d1f675d7655682b52c3b86355502c33d21
luca2005schaefer-dot <luca2005schaefer@yahoo.com>
2026-05-09 12:00:55 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+
+const app = express();
+const db = new sqlite3.Database("./gamble.db");
+
+db.run(`
+CREATE TABLE IF NOT EXISTS users (
+ username TEXT PRIMARY KEY,
+ balance INTEGER DEFAULT 100
+)
+`);
+
+app.get("/api/register", (req, res) => {
+ const user = req.query.user;
+
+ db.get("SELECT * FROM users WHERE username=?", [user], (err,row)=>{
+   if(row){
+     return res.send(`@${user} already registered`);
+   }
+
+   db.run(
+     "INSERT INTO users(username,balance) VALUES(?,?)",
+     [user,100],
+     ()=>{
+       res.send(`@${user} registered with 100 coins`);
+     }
+   );
+ });
+});
+
+app.get("/api/balance", (req,res)=>{
+ const user = req.query.user;
+
+ db.get(
+   "SELECT balance FROM users WHERE username=?",
+   [user],
+   (err,row)=>{
+     if(!row){
+       return res.send(`@${user} not registered`);
+     }
+
+     res.send(`@${user} has ${row.balance} coins`);
+   }
+ );
+});
+
+app.listen(3000, ()=>{
+ console.log("Server running on port 3000");
+});
